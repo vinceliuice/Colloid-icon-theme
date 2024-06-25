49cd09c6801b4fddc41103dce9953ede6ca6cdb7
vinceliuice <vinceliuice@hotmail.com>
2024-06-25 15:08:02 +0800

    Update

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Usage:  `./install.sh`  **[OPTIONS...]**
 ```
 -d, --dest DIR          Specify destination directory (Default: $HOME/.local/share/icons)
 -n, --name NAME         Specify theme name (Default: Colloid)
--s, --scheme TYPES      Specify folder color scheme variant(s) [default|nord|dracula|all]
+-s, --scheme VARIANTS   Specify folder colorscheme variant(s) [default|nord|dracula|gruvbox|everforest|catppuccin|all]
 -t, --theme VARIANTS    Specify folder color variant(s) [default|purple|pink|red|orange|yellow|green|teal|grey|all] (Default: blue)
 -notint, --notint       Disable Follow ColorSheme for folders on KDE Plasma
 -h, --help              Show help
@@ -24,6 +24,6 @@ Usage:  `./install.sh`  **[OPTIONS...]**
 
 > For more information, run: `./install.sh -h`
 
-![1](folder-default.png?raw=true)
-![2](folder-nord.png?raw=true)
-![3](folder-dracula.png?raw=true)
+![folders](folders.png?raw=true)
+
+![schemes](schemes.png?raw=true)
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
 set -eo pipefail
 
@@ -14,10 +14,12 @@ fi
 
 SRC_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+source "${SRC_DIR}/lib_colors.sh"
+
 THEME_NAME=Colloid
-COLOR_VARIANTS=('-light' '-dark' '')
-SCHEME_VARIANTS=('' '-nord' '-dracula')
-THEME_VARIANTS=('' '-purple' '-pink' '-red' '-orange' '-yellow' '-green' '-teal' '-grey')
+COLOR_VARIANTS=('' '-Light' '-Dark')
+THEME_VARIANTS=('' '-Purple' '-Pink' '-Red' '-Orange' '-Yellow' '-Green' '-Teal' '-Grey')
+SCHEME_VARIANTS=('' '-Nord' '-Dracula' '-Gruvbox' '-Everforest' '-Catppuccin')
 
 usage() {
 cat << EOF
@@ -26,7 +28,7 @@ cat << EOF
   OPTIONS:
     -d, --dest DIR          Specify destination directory (Default: $DEST_DIR)
     -n, --name NAME         Specify theme name (Default: $THEME_NAME)
-    -s, --scheme TYPES      Specify folder color scheme variant(s) [default|nord|dracula|all]
+    -s, --scheme VARIANTS   Specify folder colorscheme variant(s) [default|nord|dracula|gruvbox|everforest|catppuccin|all]
     -t, --theme VARIANTS    Specify folder color theme variant(s) [default|purple|pink|red|orange|yellow|green|teal|grey|all] (Default: blue)
     -notint, --notint       Disable Follow ColorSheme for folders on KDE Plasma
     -h, --help              Show help
@@ -46,81 +48,83 @@ install() {
 
   echo "Installing '${THEME_DIR}'..."
 
-  mkdir -p                                                                                   ${THEME_DIR}
-  cp -r "${SRC_DIR}"/src/index.theme                                                         ${THEME_DIR}
-  sed -i "s/Colloid/${2}${3}${4}${5}/g"                                                      ${THEME_DIR}/index.theme
+  mkdir -p                                                                                  "${THEME_DIR}"
+  cp -r "${SRC_DIR}"/src/index.theme                                                        "${THEME_DIR}"
+  sed -i "s/Colloid/${2}${3}${4}${5}/g"                                                     "${THEME_DIR}"/index.theme
 
-  if [[ "${color}" == '-light' ]]; then
-    cp -r "${SRC_DIR}"/src/{actions,apps,categories,devices,emblems,mimetypes,places,status} ${THEME_DIR}
-    cp -r "${SRC_DIR}"/links/*                                                               ${THEME_DIR}
-
-    if [[ "${theme}" != '' ]]; then
-      cp -r "${SRC_DIR}"/colors/color${theme}${scheme}/*.svg                                 ${THEME_DIR}/places/scalable
-    elif [[ ${scheme} != '' ]]; then
-      cp -r "${SRC_DIR}"/colors/color-blue${scheme}/*.svg                                    ${THEME_DIR}/places/scalable
-    fi
+  if [[ "${color}" == '-Light' ]]; then
+    cp -r "${SRC_DIR}"/src/{actions,apps,categories,devices,emblems,mimetypes,places,status} "${THEME_DIR}"
 
     if [[ "${theme}" == '' && "${scheme}" == '' && "${notint}" == 'true' ]]; then
-      cp -r "${SRC_DIR}"/notint/*.svg                                                        ${THEME_DIR}/places/scalable
+      cp -r "${SRC_DIR}"/notint/*.svg                                                       "${THEME_DIR}"/places/scalable
     fi
+
+    colors_folder
+
+    if [[ "${theme}" != '' ]]; then
+      cp -r "${SRC_DIR}"/notint/*.svg                                                       "${THEME_DIR}"/places/scalable
+      sed -i "s/#60c0f0/${theme_color}/g"                                                   "${THEME_DIR}"/places/scalable/*.svg
+    fi
+
+    cp -r "${SRC_DIR}"/links/*                                                               "${THEME_DIR}"
   fi
 
-  if [[ "${color}" == '-dark' ]]; then
-    mkdir -p                                                                                 ${THEME_DIR}/{apps,categories,devices,emblems,mimetypes,places,status}
-    cp -r "${SRC_DIR}"/src/actions                                                           ${THEME_DIR}
-    cp -r "${SRC_DIR}"/src/apps/symbolic                                                     ${THEME_DIR}/apps
-    cp -r "${SRC_DIR}"/src/categories/symbolic                                               ${THEME_DIR}/categories
-    cp -r "${SRC_DIR}"/src/emblems/symbolic                                                  ${THEME_DIR}/emblems
-    cp -r "${SRC_DIR}"/src/mimetypes/symbolic                                                ${THEME_DIR}/mimetypes
-    cp -r "${SRC_DIR}"/src/devices/{16,22,24,32,symbolic}                                    ${THEME_DIR}/devices
-    cp -r "${SRC_DIR}"/src/places/{16,22,24,symbolic}                                        ${THEME_DIR}/places
-    cp -r "${SRC_DIR}"/src/status/{16,22,24,symbolic}                                        ${THEME_DIR}/status
+  if [[ "${color}" == '-Dark' ]]; then
+    mkdir -p                                                                                "${THEME_DIR}"/{apps,categories,devices,emblems,mimetypes,places,status}
+    cp -r "${SRC_DIR}"/src/actions                                                          "${THEME_DIR}"
+    cp -r "${SRC_DIR}"/src/apps/symbolic                                                    "${THEME_DIR}"/apps
+    cp -r "${SRC_DIR}"/src/categories/symbolic                                              "${THEME_DIR}"/categories
+    cp -r "${SRC_DIR}"/src/emblems/symbolic                                                 "${THEME_DIR}"/emblems
+    cp -r "${SRC_DIR}"/src/mimetypes/symbolic                                               "${THEME_DIR}"/mimetypes
+    cp -r "${SRC_DIR}"/src/devices/{16,22,24,32,symbolic}                                   "${THEME_DIR}"/devices
+    cp -r "${SRC_DIR}"/src/places/{16,22,24,symbolic}                                       "${THEME_DIR}"/places
+    cp -r "${SRC_DIR}"/src/status/{16,22,24,symbolic}                                       "${THEME_DIR}"/status
 
     # Change icon color for dark theme
     sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/{actions,devices,places,status}/{16,22,24}/*
     sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/{actions,devices}/32/*
     sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/{actions,apps,categories,devices,emblems,mimetypes,places,status}/symbolic/*
 
-    cp -r "${SRC_DIR}"/links/actions/{16,22,24,32,symbolic}                                  ${THEME_DIR}/actions
-    cp -r "${SRC_DIR}"/links/devices/{16,22,24,32,symbolic}                                  ${THEME_DIR}/devices
-    cp -r "${SRC_DIR}"/links/places/{16,22,24,symbolic}                                      ${THEME_DIR}/places
-    cp -r "${SRC_DIR}"/links/status/{16,22,24,symbolic}                                      ${THEME_DIR}/status
-    cp -r "${SRC_DIR}"/links/apps/symbolic                                                   ${THEME_DIR}/apps
-    cp -r "${SRC_DIR}"/links/categories/symbolic                                             ${THEME_DIR}/categories
-    cp -r "${SRC_DIR}"/links/mimetypes/symbolic                                              ${THEME_DIR}/mimetypes
+    cp -r "${SRC_DIR}"/links/actions/{16,22,24,32,symbolic}                                 "${THEME_DIR}"/actions
+    cp -r "${SRC_DIR}"/links/devices/{16,22,24,32,symbolic}                                 "${THEME_DIR}"/devices
+    cp -r "${SRC_DIR}"/links/places/{16,22,24,symbolic}                                     "${THEME_DIR}"/places
+    cp -r "${SRC_DIR}"/links/status/{16,22,24,symbolic}                                     "${THEME_DIR}"/status
+    cp -r "${SRC_DIR}"/links/apps/symbolic                                                  "${THEME_DIR}"/apps
+    cp -r "${SRC_DIR}"/links/categories/symbolic                                            "${THEME_DIR}"/categories
+    cp -r "${SRC_DIR}"/links/mimetypes/symbolic                                             "${THEME_DIR}"/mimetypes
 
-    cd ${dest}
-    ln -sf ../../${name}${theme}${scheme}-light/apps/scalable ${name}${theme}${scheme}-dark/apps/scalable
-    ln -sf ../../${name}${theme}${scheme}-light/devices/scalable ${name}${theme}${scheme}-dark/devices/scalable
-    ln -sf ../../${name}${theme}${scheme}-light/places/scalable ${name}${theme}${scheme}-dark/places/scalable
-    ln -sf ../../${name}${theme}${scheme}-light/categories/32 ${name}${theme}${scheme}-dark/categories/32
-    ln -sf ../../${name}${theme}${scheme}-light/emblems/16 ${name}${theme}${scheme}-dark/emblems/16
-    ln -sf ../../${name}${theme}${scheme}-light/emblems/22 ${name}${theme}${scheme}-dark/emblems/22
-    ln -sf ../../${name}${theme}${scheme}-light/status/32 ${name}${theme}${scheme}-dark/status/32
-    ln -sf ../../${name}${theme}${scheme}-light/mimetypes/scalable ${name}${theme}${scheme}-dark/mimetypes/scalable
+    cd "${dest}"
+    ln -sf ../../"${name}${theme}${scheme}"-Light/apps/scalable "${name}${theme}${scheme}"-Dark/apps/scalable
+    ln -sf ../../"${name}${theme}${scheme}"-Light/devices/scalable "${name}${theme}${scheme}"-Dark/devices/scalable
+    ln -sf ../../"${name}${theme}${scheme}"-Light/places/scalable "${name}${theme}${scheme}"-Dark/places/scalable
+    ln -sf ../../"${name}${theme}${scheme}"-Light/categories/32 "${name}${theme}${scheme}"-Dark/categories/32
+    ln -sf ../../"${name}${theme}${scheme}"-Light/emblems/16 "${name}${theme}${scheme}"-Dark/emblems/16
+    ln -sf ../../"${name}${theme}${scheme}"-Light/emblems/22 "${name}${theme}${scheme}"-Dark/emblems/22
+    ln -sf ../../"${name}${theme}${scheme}"-Light/status/32 "${name}${theme}${scheme}"-Dark/status/32
+    ln -sf ../../"${name}${theme}${scheme}"-Light/mimetypes/scalable "${name}${theme}${scheme}"-Dark/mimetypes/scalable
   fi
 
   if [[ "${color}" == '' ]]; then
-    mkdir -p                                                                                 ${THEME_DIR}/status
-    cp -r "${SRC_DIR}"/src/status/{16,22,24}                                                 ${THEME_DIR}/status
+    mkdir -p                                                                                "${THEME_DIR}"/status
+    cp -r "${SRC_DIR}"/src/status/{16,22,24}                                                "${THEME_DIR}"/status
     # Change icon color for dark panel
     sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/status/{16,22,24}/*
-    cp -r "${SRC_DIR}"/links/status/{16,22,24}                                               ${THEME_DIR}/status
+    cp -r "${SRC_DIR}"/links/status/{16,22,24}                                              "${THEME_DIR}"/status
 
     cd ${dest}
-    ln -sf ../${name}${theme}${scheme}-light/apps ${name}${theme}${scheme}/apps
-    ln -sf ../${name}${theme}${scheme}-light/actions ${name}${theme}${scheme}/actions
-    ln -sf ../${name}${theme}${scheme}-light/devices ${name}${theme}${scheme}/devices
-    ln -sf ../${name}${theme}${scheme}-light/emblems ${name}${theme}${scheme}/emblems
-    ln -sf ../${name}${theme}${scheme}-light/places ${name}${theme}${scheme}/places
-    ln -sf ../${name}${theme}${scheme}-light/categories ${name}${theme}${scheme}/categories
-    ln -sf ../${name}${theme}${scheme}-light/mimetypes ${name}${theme}${scheme}/mimetypes
-    ln -sf ../../${name}${theme}${scheme}-light/status/32 ${name}${theme}${scheme}/status/32
-    ln -sf ../../${name}${theme}${scheme}-light/status/symbolic ${name}${theme}${scheme}/status/symbolic
+    ln -sf ../"${name}${theme}${scheme}"-Light/apps "${name}${theme}${scheme}"/apps
+    ln -sf ../"${name}${theme}${scheme}"-Light/actions "${name}${theme}${scheme}"/actions
+    ln -sf ../"${name}${theme}${scheme}"-Light/devices "${name}${theme}${scheme}"/devices
+    ln -sf ../"${name}${theme}${scheme}"-Light/emblems "${name}${theme}${scheme}"/emblems
+    ln -sf ../"${name}${theme}${scheme}"-Light/places "${name}${theme}${scheme}"/places
+    ln -sf ../"${name}${theme}${scheme}"-Light/categories "${name}${theme}${scheme}"/categories
+    ln -sf ../"${name}${theme}${scheme}"-Light/mimetypes "${name}${theme}${scheme}"/mimetypes
+    ln -sf ../../"${name}${theme}${scheme}"-Light/status/32 "${name}${theme}${scheme}"/status/32
+    ln -sf ../../"${name}${theme}${scheme}"-Light/status/symbolic "${name}${theme}${scheme}"/status/symbolic
   fi
 
   (
-    cd ${THEME_DIR}
+    cd "${THEME_DIR}"
     ln -sf actions actions@2x
     ln -sf apps apps@2x
     ln -sf categories categories@2x
@@ -131,7 +135,7 @@ install() {
     ln -sf status status@2x
   )
 
-  gtk-update-icon-cache ${THEME_DIR}
+  gtk-update-icon-cache "${THEME_DIR}"
 }
 
 while [[ "$#" -gt 0 ]]; do
@@ -147,6 +151,7 @@ while [[ "$#" -gt 0 ]]; do
       ;;
     -notint|--notint)
       notint='true'
+      echo -e "\nInstall notint version! that folders will not follow system colorschemes..."
       shift
       ;;
     -s|--scheme)
@@ -159,14 +164,32 @@ while [[ "$#" -gt 0 ]]; do
             ;;
           nord)
             schemes+=("${SCHEME_VARIANTS[1]}")
+            echo -e "\nNord ColorScheme version! ..."
             shift
             ;;
           dracula)
             schemes+=("${SCHEME_VARIANTS[2]}")
+            echo -e "\nDracula ColorScheme version! ..."
+            shift
+            ;;
+          gruvbox)
+            schemes+=("${SCHEME_VARIANTS[3]}")
+            echo -e "\nGruvbox ColorScheme version! ..."
+            shift
+            ;;
+          everforest)
+            schemes+=("${SCHEME_VARIANTS[4]}")
+            echo -e "\nEverforest ColorScheme version! ..."
+            shift
+            ;;
+          catppuccin)
+            schemes+=("${SCHEME_VARIANTS[5]}")
+            echo -e "\nCatppuccin ColorScheme version! ..."
             shift
             ;;
           all)
             schemes+=("${SCHEME_VARIANTS[@]}")
+            echo -e "\All ColorSchemes version! ..."
             shift
             ;;
           -*|--*)
@@ -259,6 +282,16 @@ if [[ "${#colors[@]}" -eq 0 ]]; then
   colors=("${COLOR_VARIANTS[@]}")
 fi
 
+clean_old_theme() {
+  for theme in '' '-purple' '-pink' '-red' '-orange' '-yellow' '-green' '-teal' '-grey'; do
+    for scheme in '' '-nord' '-dracula'; do
+      for color in '' '-light' '-dark'; do
+        rm -rf "${DEST_DIR}/${THEME_NAME}${theme}${scheme}${color}"
+      done
+    done
+  done
+}
+
 install_theme() {
   for theme in "${themes[@]}"; do
     for scheme in "${schemes[@]}"; do
@@ -269,4 +302,4 @@ install_theme() {
   done
 }
 
-install_theme
+clean_old_theme && install_theme
